2c406bfb55ae73a390e5b2b111cb43268eb634f0
Le Van Cong <lecongdtvt10@gmail.com>
2022-05-27 23:09:43 +0900

    2-bar-chart

---
+++ 2-bar-chart/src/data.ts
@@ -0,0 +1,47 @@
+export interface ResultEntry {
+  party: string;
+  seats: number;
+}
+
+export const data: ResultEntry[] = [
+  {
+    party: "ERC",
+    seats: 13
+  },
+  {
+    party: "Cs",
+    seats: 10
+  },
+  {
+    party: "JxCat",
+    seats: 8
+  },
+  {
+    party: "PNV",
+    seats: 7
+  },
+  {
+    party: "Bildu",
+    seats: 5
+  },
+  {
+    party: "Más pais",
+    seats: 4
+  },
+  {
+    party: "CUP",
+    seats: 2
+  },
+  {
+    party: "CC",
+    seats: 2
+  },
+  {
+    party: "BNG",
+    seats: 1
+  },
+  {
+    party: "Teruel Existe",
+    seats: 1
+  }
+];
